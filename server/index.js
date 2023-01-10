c413a957d3cc8363d505e0a0c6b57afe53cdd09a
Francisco Vaninetti <franciscovaninetti@gmail.com>
2023-01-10 02:10:20 -0300

    Setting dotenv for env variables

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import tiresRoutes from './routes/Tires.js';
 
-const CONNECTION_URL = 'mongodb+srv://franciscoVaninetti:franciscoVaninetti33@cluster0.xzclww5.mongodb.net/?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://franciscoVaninetti:franciscoVaninetti33@cluster0.xzclww5.mongodb.net/?retryWrites=true&w=majority'
 
 const PORT = process.env.PORT | 5000;
 
 const app = express();
+dotenv.config();
 
 app.use('/tires', tiresRoutes);
 
@@ -17,7 +19,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-mongoose.connect(CONNECTION_URL, { logger: true })
+mongoose.connect(process.env.CONNECTION_URL, { logger: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
     .catch((error) => console.error(error.message));
 
